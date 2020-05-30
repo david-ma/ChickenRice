6a8270e204c6f93164f53ef50a1e8f93932f1b51
David Ma <hi@david-ma.net>
2020-05-30 18:16:22 +1000

    Post a hello world image of chicken

--- index.js
+++ index.js
@@ -23,37 +23,52 @@
 
 require('dotenv').config()
 const Twitter = require('twitter-lite');
-const fs = require('fs').promises;
-
-const client = new Twitter({
-    subdomain: "api",
-    version: "1.1",
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-});
+const fs = require('fs');
 
 
+function newClient(subdomain = 'api') {
+    return new Twitter({
+        subdomain: subdomain,
+        version: "1.1",
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    });
+}
+
+var status = `Hello World! Here's your first serving of chicken rice :)`;
+
+var image = fs.readFileSync('new_images/hainanese-chicken-lg-6911.jpg', { encoding: 'base64' });
 
 
-client
-    .get("account/verify_credentials")
+newClient('upload')
+    .post("media/upload", {
+        media_data: image,
+        status: status
+    })
     .then(results => {
 
         console.log("Success!!!!");
         console.log("results", results);
 
+        newClient().post('statuses/update', {
+            status: status,
+            media_ids: [results.media_id_string]
+        }).then(result => {
+            console.log("finish");
+            console.log(result);
+        }).catch(err => console.log(err));
+        
     })
     .catch(console.error);
 
 
-fs.readdir(`new_images/`)
+/*
+fs.promises.readdir(`new_images/`)
     .then( function( files ) {
         console.log(files);
     });
-
-
-
+*/
 
 
